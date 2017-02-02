515461230d4b76131fe89ba1c7549f66936b3a00
Elsa Collet <collet.e@hotmail.fr>
2017-02-02 12:15:57 +0100

    Elsa 2/02/17 delete FLoorView unused attributs

--- src/mariaLost/gamePlay/view/FloorView.java
+++ src/mariaLost/gamePlay/view/FloorView.java
@@ -13,13 +13,9 @@ import java.util.ArrayList;
  */
 public class FloorView {
 
-    private Canvas canvas;
-    private int lengthX;
-    private int lengthY;
+
     private ArrayList<Item> floorList;
     public FloorView(GroundFloor groundFloor){
-        this.lengthX = groundFloor.getLengthX();
-        this.lengthY = groundFloor.getLengthY();
         floorList = new ArrayList<>();
         readGroundFloor(groundFloor);
     }
@@ -29,35 +25,17 @@ public class FloorView {
         int x = 0;
         int y= 0;
         int pieceSize=groundFloor.getItemFromFloor(0,0).getSize();
-        this.canvas=new Canvas(groundFloor.getLengthX(),groundFloor.getLengthY());
-        this.canvas.setTranslateX(0);
-        this.canvas.setTranslateY(0);
-        canvas.setHeight(groundFloor.getLengthX()*50);
-        canvas.setWidth(groundFloor.getLengthX()*50);
 
         System.out.println(groundFloor.getNbCaseX());
         for(int i=0; i<groundFloor.getNbCaseX(); i++){
             for(int j=0; j<groundFloor.getNbCaseY(); j++){
                 Item item= groundFloor.getItemFromFloor(i,j);
                 this.floorList.add(item);
-
-          /*      GraphicsContext gc=canvas.getGraphicsContext2D();
-                System.out.println("item : "+ item.getSpriteName());
-                Image im= new Image("file:resources/Images/"+item.getSpriteName());
-                gc.drawImage(im,x,y);
-*/
                 x+=pieceSize;
             }
             x=0;
             y+=pieceSize;
         }
-    }
-    public void readGroundFloor2(){
-
-    }
-
-    public Canvas getCanvas() {
-        return canvas;
     }
 
 }
