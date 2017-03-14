5cd8437bd248dd698f59719ead41f56acc583530
Lumen <credeville.louis.maxime@gmail.com>
2017-03-14 14:17:39 +0100

    Modification pour pouvoir tirée des boule de feu dans toute les direction pas uniquement dans les 8 définie

--- src/mariaLost/items/model/FireballAttack.java
+++ src/mariaLost/items/model/FireballAttack.java
@@ -6,53 +6,54 @@ import javafx.util.Duration;
 import mariaLost.gamePlay.tools.Direction;
 import mariaLost.items.model.animation.AnimationFireball;
 
-public class FireballAttack{
+public class FireballAttack {
 
-public static RangedAttack getFireball(Point2D startingPoint,Direction direction){
-		Dimension2D dimension;
-		if(direction.equals(Direction.RIGHT)){
-			dimension=new Dimension2D(40,15);
-			startingPoint=new Point2D(startingPoint.getX()
-					,startingPoint.getY()-dimension.getHeight()/2);
-		}else if(direction.equals(Direction.LEFT)){
-			dimension=new Dimension2D(40,15);
-			startingPoint=new Point2D(startingPoint.getX()-dimension.getWidth()
-					,startingPoint.getY()-dimension.getHeight()/2);
-		}else if(direction.equals(Direction.UP)){
-			dimension=new Dimension2D(15,40);
-			startingPoint=new Point2D(startingPoint.getX()-dimension.getWidth()/2
-					,startingPoint.getY()-dimension.getHeight());
-		}else if(direction.equals(Direction.DOWN)){
-			dimension=new Dimension2D(15,40);
-			startingPoint=new Point2D(startingPoint.getX()-dimension.getWidth()/2
-					,startingPoint.getY());
+    public static RangedAttack getFireball(Point2D startingPoint, Direction direction) {
 
-			
-			
-			
-			
-		}else if(direction.equals(Direction.UP_RIGHT)){
-			dimension=new Dimension2D(40,20);
-			startingPoint=new Point2D(startingPoint.getX()
-					,startingPoint.getY()-dimension.getHeight());
+        Direction speed = direction;
+        direction = direction.adjust();
 
-		}else if(direction.equals(Direction.UP_LEFT)){
-			dimension=new Dimension2D(40,20);
-			startingPoint=new Point2D(startingPoint.getX()-dimension.getWidth()
-					,startingPoint.getY()-dimension.getHeight());
-	
-		}else if(direction.equals(Direction.DOWN_RIGHT)){
-			dimension=new Dimension2D(40,20);
-
-		}else{ 
-			dimension=new Dimension2D(40,20);
-			startingPoint=new Point2D(startingPoint.getX()-dimension.getWidth()
-					,startingPoint.getY());
-		}
-
-	return new RangedAttack(startingPoint, dimension, direction, 40, AnimationFireball.getFireball(direction), new Duration(3000), 2);
+        Dimension2D dimension;
+        if (direction.equals(Direction.RIGHT)) {
+            dimension = new Dimension2D(40, 15);
+            startingPoint = new Point2D(startingPoint.getX()
+                    , startingPoint.getY() - dimension.getHeight() / 2);
+        } else if (direction.equals(Direction.LEFT)) {
+            dimension = new Dimension2D(40, 15);
+            startingPoint = new Point2D(startingPoint.getX() - dimension.getWidth()
+                    , startingPoint.getY() - dimension.getHeight() / 2);
+        } else if (direction.equals(Direction.UP)) {
+            dimension = new Dimension2D(15, 40);
+            startingPoint = new Point2D(startingPoint.getX() - dimension.getWidth() / 2
+                    , startingPoint.getY() - dimension.getHeight());
+        } else if (direction.equals(Direction.DOWN)) {
+            dimension = new Dimension2D(15, 40);
+            startingPoint = new Point2D(startingPoint.getX() - dimension.getWidth() / 2
+                    , startingPoint.getY());
 
 
-}
-	
+        } else if (direction.equals(Direction.UP_RIGHT)) {
+            dimension = new Dimension2D(40, 20);
+            startingPoint = new Point2D(startingPoint.getX()
+                    , startingPoint.getY() - dimension.getHeight());
+
+        } else if (direction.equals(Direction.UP_LEFT)) {
+            dimension = new Dimension2D(40, 20);
+            startingPoint = new Point2D(startingPoint.getX() - dimension.getWidth()
+                    , startingPoint.getY() - dimension.getHeight());
+
+        } else if (direction.equals(Direction.DOWN_RIGHT)) {
+            dimension = new Dimension2D(40, 20);
+
+        } else {
+            dimension = new Dimension2D(40, 20);
+            startingPoint = new Point2D(startingPoint.getX() - dimension.getWidth()
+                    , startingPoint.getY());
+        }
+
+        return new RangedAttack(startingPoint, dimension, speed, 40, AnimationFireball.getFireball(direction), new Duration(3000), 2);
+
+
+    }
+
 }
